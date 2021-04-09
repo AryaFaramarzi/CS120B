fc26197bf95b89a84eb89f92e717a0a828bc3e9b
Arya Faramarzi <afara017@wch136-24.cs.ucr.edu>
2021-04-09 12:00:51 -0700

    saving progress

--- Lab3_BitManipulation/source/main.c
+++ Lab3_BitManipulation/source/main.c
@@ -1,12 +1,13 @@
 /*	Author: afara017
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section: 023
+ *	Assignment: Lab 3  Exercise 1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
+
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
@@ -14,10 +15,19 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
+	DDRB = 0xFE; PORTB = 0x00;
+	DDRD = 0x00; PORTD = 0x00;
 
+	unsigned int w = 0;
     /* Insert your solution below */
     while (1) {
+    	w = PIND;
+	w = w << 1;
+	w += (PINB & 0x01);
 
+	if(w >= 70) {
+		PORTB = 0x02;
+	}
     }
     return 1;
 }
