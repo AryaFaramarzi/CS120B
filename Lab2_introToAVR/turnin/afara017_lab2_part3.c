608728991e569cfb96665c8500e7e79b0a546392
Arya Faramarzi <afara017@wch136-24.cs.ucr.edu>
2021-04-08 22:50:38 -0700

    Moving stuff around

---
+++ Lab2_introToAVR/turnin/afara017_lab2_part3.c
@@ -0,0 +1,33 @@
+/*	Author: afara017
+ *  Partner(s) Name: 
+ *	Lab Section: 023
+ *	Assignment: Lab 2  Exercise 3
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0x00;
+	DDRC = 0xFF; PORTC = 0x00;
+	
+	unsigned char tmpC = 0x00;
+    /* Insert your solution below */
+    while (1) {
+	tmpC = !(PINA & 0x01) + !((PINA & 0x02) >> 1)+ !((PINA & 0x04) >> 2) + !((PINA & 0x08) >> 3); 	
+	if(tmpC == 0x00) {
+		PORTC = 0x80;
+	}
+	else {
+		PORTC = tmpC;
+	}
+    }
+    return 1;
+}
