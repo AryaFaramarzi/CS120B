0abce78e6c73413094e327d50c50138b18ce3e26
AryaFaramarzi <afara017@ucr.edu>
2021-05-29 22:41:53 -0700

    First draft of the proj

--- Lab11-Final/source/main.c
+++ Lab11-Final/source/main.c
@@ -8,24 +8,35 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include <stdlib.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #include "scheduler.h"
 #include "bit.h"
 #include "timer.h"
 #endif
-//--------------------------------------
-// LED Matrix Demo SynchSM
-// Period: 100 ms
-//--------------------------------------
+// global vars
+// Dinasour position
+unsigned char dRow, dCol;
+
+// Wall position
+unsigned char wRow, wCol;
+// Wall orientation (up / down)
+unsigned char orient;
+// Wall on led-matrix
+unsigned char wall_present;
+
+// Game started/ended
+unsigned char game;
+
+// High-score
+unsigned int high_score;
+
+// Buttons
+unsigned char up, down;
+
+
 enum Demo_States {shift};
-int test(int state) {
-		static unsigned char pattern = 0x80;
-		static unsigned char row = 0xfe;
-		PORTC = pattern;	// Pattern to display
-		PORTD = row;		// Row(s) displaying pattern	
-		return state;
-}
 int Demo_Tick(int state) {
 
 	// Local Variables
@@ -63,7 +74,75 @@ int Demo_Tick(int state) {
 }
 
 
+enum Dinasour_States {Begin_Dinasour, Wait_Dinasour, Jump_Dinasour, Duck_Dinasour};
+int Dinasour(int state) {
+		up = ((~PINA & 0x01) == 0x01); // PA0
+		down = ((~PINA & 0x04) == 0x04); // PA2
+		switch(state) {
+				case Begin_Dinasour:
+					if(up)
+						state = Wait_Dinasour;
+					else
+						state = Begin_Dinasour;
+					break;
+				case Wait_Dinasour:
+					break;
+				case Jump_Dinasour:
+					state = Wait_Dinasour;
+					break;
+				case Duck_Dinasour:
+					state = Wait_Dinasour;
+					break;
+				default:
+					state = Begin_Dinasour;
+					break;
+		}
+		switch(state) {
+				case Begin_Dinasour:
+					dRow = 3;
+					dCol = 2;
+					game = 0;
+					break;
+				case Wait_Dinasour:
+					game = 1;
+					dRow = 3;
+					dCol= 2;
+					break;
+				case Jump_Dinasour:
+					dRow = 4;
+					dCol = 2;
+					break;
+				case Duck_Dinasour:
+					dRow = 2;
+					dCol = 2;
+					break;
+		}
+		return state;
+}
+
+enum Wall_States {Begin_Wall, Create_Wall, Move_Wall, };
+int Wall(int state)
+{
+	return state;
+}
+
+//enum Score_States {};
+int Score(int state)
+{
+	return state;
+}
+
+//enum Display_states {};
+int Display(int state)
+{
+	return state;	
+}
+
+
+
 int main(void) {
+	// seed the rand function
+	srand(time(0));
     /* Insert DDR and PORT initializations */
 	DDRC = 0xff; PORTC = 0x00;
 	DDRD = 0xff; PORTD = 0x00;
@@ -77,7 +156,7 @@ int main(void) {
     task1.state = start;
     task1.period = 100;
     task1.elapsedTime = task1.period;
-    task1.TickFct = &test;
+    task1.TickFct = &Display;
     
     unsigned short i;
     unsigned long GCD = tasks[0]->period;
